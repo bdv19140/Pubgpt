8d5794ff799a6381795e726be76d3214683b6c60
bdv19140 <contact@brasserie-vezere.fr>
2025-03-30 22:36:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,3 +104,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
